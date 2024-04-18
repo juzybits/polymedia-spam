9e0143d5c1acfd07709567173d2768945c47f5b3
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-18 12:17:49 +0000

    js: create monorepo

---
+++ .eslintrc.cjs
@@ -0,0 +1,57 @@
+module.exports = {
+    root: true,
+    env: {},
+    ignorePatterns: ["dist", "node_modules"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: [ "./tsconfig.json" ],
+        tsconfigRootDir: __dirname,
+    },
+    plugins: [
+        "@stylistic",
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+    ],
+    rules: {
+        "@stylistic/quotes": [ "error", "double", { "avoidEscape": true } ],
+        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        "@typescript-eslint/no-confusing-void-expression": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/prefer-nullish-coalescing": [ "error", { "ignoreConditionalTests": true } ],
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/use-unknown-in-catch-callback-variable": "off",
+    },
+    overrides: [
+        {
+            files: ["src/cli/**/*"],
+            env: { node: true },
+        },
+        {
+            files: ["src/react/**/*", "src/web/**/*"],
+            env: { browser: true },
+            plugins: ["react-refresh"],
+            extends: [
+                "plugin:react-hooks/recommended",
+                "plugin:react/jsx-runtime",
+                "plugin:react/recommended",
+            ],
+            rules: {
+                "react-hooks/exhaustive-deps": "off",
+                "react/no-unescaped-entities": "off",
+                "react/prop-types": "off",
+                "react/react-in-jsx-scope": "off",
+            },
+            settings: {
+                react: {
+                    version: "detect"
+                }
+            },
+        },
+    ],
+};
