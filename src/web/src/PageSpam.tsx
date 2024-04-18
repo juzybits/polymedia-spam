0871c4ecd9dc5f8f1b32faa6195f136169f5a76f
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-18 18:25:05 +0000

    web: /spam: derive keypair

--- src/web/src/PageSpam.tsx
+++ src/web/src/PageSpam.tsx
@@ -1,14 +1,22 @@
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { AppContext } from "./App";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { decodeSuiPrivateKey } from "@mysten/sui.js/cryptography";
 
 export const PageSpam: React.FC = () =>
 {
-    const { wallet } = useOutletContext<AppContext>();
     const navigate = useNavigate();
+    const { wallet } = useOutletContext<AppContext>();
+    const [ pair, setPair ] = useState<Ed25519Keypair|null>(null);
 
     useEffect(() => {
-        !wallet && navigate("/user");
+        if (!wallet) {
+            navigate("/user")
+        } else {
+            const parsedPair = decodeSuiPrivateKey(wallet.secretKey);
+            setPair(Ed25519Keypair.fromSecretKey(parsedPair.secretKey));
+        }
     }, [wallet])
 
     return <div id="page-content" >
