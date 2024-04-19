06ba4a28c3169e3500b0ab5fcea80df8a421cc9e
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-19 17:51:54 +0000

    Revert "sdk: SpamClient"
    This reverts commit b505576c104550db0da8ef73503dc45ca1a0535f.

--- src/sdk/src/index.ts
+++ src/sdk/src/index.ts
@@ -1,4 +1,4 @@
 export * from "./config.js";
-export * from "./client.js";
-export * from "./package.js";
+export * from "./functions-client.js";
+export * from "./functions-package.js";
 export * from "./types.js";
