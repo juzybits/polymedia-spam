dc6d0e2756a1c4aaaffe251ba73a43c9424ab7c4
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-19 16:37:09 +0000

    web: /spam: basic spam() method, error handling

--- src/web/src/PageSpam.tsx
+++ src/web/src/PageSpam.tsx
@@ -5,6 +5,7 @@ import { SPAM_IDS, UserCounter, fetchUserCounters } from "@polymedia/spam-sdk";
 import { useEffect, useState } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { AppContext } from "./App";
+import { ErrorBox } from "./components/ErrorBox";
 
 export const PageSpam: React.FC = () =>
 {
@@ -15,8 +16,11 @@ export const PageSpam: React.FC = () =>
     const { network, wallet } = useOutletContext<AppContext>();
     const [ pair, setPair ] = useState<Ed25519Keypair|null>(null);
     const [ counters, setCounters ] = useState<UserCounter[]>();
+    const [ isRunning, setIsRunning ] = useState<boolean>(false);
+    const [ error, setError ] = useState<string|null>(null);
 
     const spamIds = SPAM_IDS[network];
+    const isLoading = !pair || !counters;
 
     /* Functions */
 
@@ -28,31 +32,80 @@ export const PageSpam: React.FC = () =>
                 return;
             }
 
-            const parsedPair = decodeSuiPrivateKey(wallet.secretKey);
-            const newPair = Ed25519Keypair.fromSecretKey(parsedPair.secretKey);
-            setPair(newPair);
+            try {
+                const parsedPair = decodeSuiPrivateKey(wallet.secretKey);
+                const newPair = Ed25519Keypair.fromSecretKey(parsedPair.secretKey);
+                setPair(newPair);
 
-            const newCounters = await fetchUserCounters(
-                suiClient,
-                spamIds.packageId,
-                newPair.toSuiAddress(),
-            );
-            setCounters(newCounters);
+                const newCounters = await fetchUserCounters(
+                    suiClient,
+                    spamIds.packageId,
+                    newPair.toSuiAddress(),
+                );
+                setCounters(newCounters);
+            } catch(err) {
+                setError(String(err));
+            }
         };
         initialize();
     }, [wallet]);
 
+    const spam = async() => {
+        if (isRunning || isLoading) {
+            return;
+        }
+        setIsRunning(true);
+        try {
+            const suiState = await suiClient.getLatestSuiSystemState();
+            const currEpoch = Number(suiState.epoch);
+
+            let currCounter: UserCounter|null;
+            let registerCounters: UserCounter[]= [];
+            let claimCounters: UserCounter[] = [];
+
+            for (const counter of counters) {
+                if (counter.epoch === currEpoch) {
+                    currCounter = counter;
+                } else if (counter.epoch == currEpoch - 1) {
+                    registerCounters.push(counter);
+                } else if (counter.epoch <= currEpoch - 2) {
+                    claimCounters.push(counter);
+                } else {
+                    throw new Error("UserCounter.epoch is newer than network epoch");
+                }
+            }
+
+            for (const counter of claimCounters) {
+                // TODO claim()
+            }
+
+            for (const counter of registerCounters) {
+                // TODO register()
+            }
+
+            const spamAmount = 5; // TODO make infinite
+            for (let i = 0; i < spamAmount; i++) {
+                // TODO increment_user_counter()
+            }
+        } catch(err) {
+            setError(String(err));
+        } finally {
+            setIsRunning(false);
+        }
+    };
+
     /* HTML */
 
-    const isLoading = !pair || !counters;
-
     let content;
-    if (isLoading) {
+    if (error) {
+        content = <ErrorBox err={error} />
+    } else if (isLoading) {
         content = <p>Loading...</p>;
     } else {
-        content = <p>
-            Counters: {counters.length}
-        </p>;
+        content = <div>
+            <p>Counters: {counters.length}</p>
+            <button className="btn" onClick={spam}>SPAM</button>
+        </div>;
     }
 
     return <div id="page-content" >
---
+++ src/web/src/components/ErrorBox.tsx
@@ -0,0 +1,14 @@
+export const ErrorBox: React.FC<{
+    err: Error|string|null,
+}> = ({
+    err,
+}) =>
+{
+    if (!err) {
+        return null;
+    }
+    return <div className='error-box'>
+        <div>Something went wrong:</div>
+        <div>{String(err)}</div>
+    </div>;
+}
