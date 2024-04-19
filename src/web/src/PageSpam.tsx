3a310f37ca738d9b2a0bcd917863e3e6bcbc8857
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-19 17:31:44 +0000

    web: /spam: Status

--- src/web/src/PageSpam.tsx
+++ src/web/src/PageSpam.tsx
@@ -7,6 +7,18 @@ import { useNavigate, useOutletContext } from "react-router-dom";
 import { AppContext } from "./App";
 import { ErrorBox } from "./components/ErrorBox";
 
+type Status =
+    "booting up" |
+    "loading key pair" |
+    "fetching counters" |
+    "ready to spam" |
+    "fetching epoch" |
+    "processing counters" |
+    "claiming counters" |
+    "registering counters" |
+    "creating counter" |
+    "spamming";
+
 export const PageSpam: React.FC = () =>
 {
     /* State */
@@ -16,7 +28,7 @@ export const PageSpam: React.FC = () =>
     const { network, wallet } = useOutletContext<AppContext>();
     const [ pair, setPair ] = useState<Ed25519Keypair|null>(null);
     const [ counters, setCounters ] = useState<UserCounter[]>();
-    const [ isRunning, setIsRunning ] = useState<boolean>(false);
+    const [ status, setStatus ] = useState<Status>("booting up");
     const [ error, setError ] = useState<string|null>(null);
 
     const spamIds = SPAM_IDS[network];
@@ -33,16 +45,19 @@ export const PageSpam: React.FC = () =>
             }
 
             try {
+                setStatus("loading key pair");
                 const parsedPair = decodeSuiPrivateKey(wallet.secretKey);
                 const newPair = Ed25519Keypair.fromSecretKey(parsedPair.secretKey);
                 setPair(newPair);
 
+                setStatus("fetching counters");
                 const newCounters = await fetchUserCounters(
                     suiClient,
                     spamIds.packageId,
                     newPair.toSuiAddress(),
                 );
                 setCounters(newCounters);
+                setStatus("ready to spam");
             } catch(err) {
                 setError(String(err));
             }
@@ -51,22 +66,23 @@ export const PageSpam: React.FC = () =>
     }, [wallet]);
 
     const spam = async() => {
-        if (isRunning || isLoading) {
+        if (status !== "ready to spam" || isLoading) {
             return;
         }
-        setIsRunning(true);
         try {
+            setStatus("fetching epoch");
             const suiState = await suiClient.getLatestSuiSystemState();
             const currEpoch = Number(suiState.epoch);
 
-            let currCounter: UserCounter|null;
+            setStatus("processing counters")
+            let currCounter: UserCounter|null = null;
             let registerCounters: UserCounter[]= [];
             let claimCounters: UserCounter[] = [];
 
             for (const counter of counters) {
                 if (counter.epoch === currEpoch) {
-                    currCounter = counter;
                 } else if (counter.epoch == currEpoch - 1) {
+                    currCounter = counter;
                     registerCounters.push(counter);
                 } else if (counter.epoch <= currEpoch - 2) {
                     claimCounters.push(counter);
@@ -75,41 +91,42 @@ export const PageSpam: React.FC = () =>
                 }
             }
 
+            claimCounters.length && setStatus("claiming counters")
             for (const counter of claimCounters) {
                 // TODO claim()
             }
 
+            registerCounters.length && setStatus("registering counters")
             for (const counter of registerCounters) {
                 // TODO register()
             }
 
+            !currCounter && setStatus("creating counter");
+
+            setStatus("spamming")
             const spamAmount = 5; // TODO make infinite
             for (let i = 0; i < spamAmount; i++) {
+                i**i/i;
                 // TODO increment_user_counter()
             }
+            setStatus("ready to spam");
         } catch(err) {
             setError(String(err));
-        } finally {
-            setIsRunning(false);
         }
     };
 
     /* HTML */
 
-    let content;
-    if (error) {
-        content = <ErrorBox err={error} />
-    } else if (isLoading) {
-        content = <p>Loading...</p>;
-    } else {
-        content = <div>
-            <p>Counters: {counters.length}</p>
-            <button className="btn" onClick={spam}>SPAM</button>
-        </div>;
-    }
-
     return <div id="page-content" >
         <h1>Spam</h1>
-        {content}
+        <div>
+            <ErrorBox err={error} />
+            <p>Status: {status}</p>
+            {isLoading
+            ? <p>Loading...</p>
+            : <p>Counters: {counters.length}</p>
+            }
+            <button className="btn" onClick={spam}>SPAM</button>
+        </div>
     </div>;
 }
