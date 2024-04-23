ab32b390b17043902f9cbbf8a9a29ad9bea3ba4a
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-23 16:15:48 +0000

    /user: wallet details and warnings

--- src/web/src/PageUser.tsx
+++ src/web/src/PageUser.tsx
@@ -7,15 +7,42 @@ export const PageUser: React.FC = () =>
     const { replaceKeypair, spammer } = useOutletContext<AppContext>();
 
     const onCreateWallet = () => {
-        replaceKeypair(new Ed25519Keypair());
+        const resp = window.confirm(
+            "🚨 WARNING 🚨\n\nThis will delete and replace your current wallet.\n\nAre you sure?"
+        );
+        if (resp) {
+            replaceKeypair(new Ed25519Keypair());
+        }
     };
 
     return <>
         <h1>User</h1>
-        <p>Your address is: {spammer.client.signer.toSuiAddress()}</p>
+
+        <p>
+            Your public address:<br/>
+            <span className="break-all">
+                {spammer.client.signer.toSuiAddress()}
+            </span>
+        </p>
+        <p>
+            Your secret key:<br/>
+            <span className="break-all">
+                {(spammer.client.signer as Ed25519Keypair).getSecretKey()}
+            </span>
+        </p>
+
+        <br/>
+        <h3>Back up your secret key!</h3>
+        <p>
+            - Your spam wallet is stored in your browser, only you have access to it.<br/>
+            - Clearing cookies will delete your wallet, and we cannot recover it for you.<br/>
+            - Copy your secret key and keep it safe, this allows you to restore your wallet.<br/>
+        </p>
+
+        <br/>
         <div className="btn-group">
-            <button className="btn" onClick={onCreateWallet}>CREATE WALLET</button>
-            <button className="btn">IMPORT WALLET</button> {/* TODO */}
+            <button className="btn" onClick={onCreateWallet}>NEW WALLET</button>
+            <button className="btn">IMPORT</button> {/* TODO */}
         </div>
     </>;
 };
