fb20ddb50acdd761fd2b8ad41f3f556da95bd1a4
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-24 19:31:33 +0000

    sdk: use LOL package on testnet

--- src/sdk/src/config.ts
+++ src/sdk/src/config.ts
@@ -11,8 +11,12 @@ export const SPAM_IDS: Record<NetworkName, NetworkConfig> = {
         directorId: "",
     },
     testnet: {
-        packageId: "",
-        directorId: "",
+        // LOL. Change SPAM_MODULE and SPAM_SYMBOL below for this to work.
+        packageId: "0x7f564f428e939e938dbdccfcc658c78ab9c5b3a2fce912138bdeeca0702446ce",
+        directorId: "0x4a3d63044b755a224fde81bbead66bd4faf6bb8a88f24dcd21e0b047d663dd67",
+        // adminCapId: "0x2010974ce9ba8d6051f78b4e9e07ac4e043ca9df4d1ebd44a429a2d1f22a6f79",
+        // upgradeCapId: "0x24f92c47ea6b9d33e2e61f5018e88f46a03cd0afe89144c2f75b52067bb0c695",
+        // coinMetadataId: "0x27dc2fafa451725a085906a2eed0dc1375ca0e69eaa6760450d6293a01709294",
     },
     devnet: {
         packageId: "",
@@ -24,7 +28,7 @@ export const SPAM_IDS: Record<NetworkName, NetworkConfig> = {
     },
 };
 
-export const SPAM_MODULE = "spam";
-export const SPAM_SYMBOL = "SPAM";
+export const SPAM_MODULE = "lol";
+export const SPAM_SYMBOL = "LOL";
 export const SPAM_DECIMALS = 4;
 export const SUI_DECIMALS = 9;
