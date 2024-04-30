8377075ca66921bd0e82f9e37d6aa4e6d89f9bd4
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-30 19:34:34 +0000

    sui: publish to testnet and devnet

--- src/sdk/src/config.ts
+++ src/sdk/src/config.ts
@@ -12,22 +12,20 @@ export const SPAM_IDS: Record<NetworkName, NetworkConfig> = {
         directorId: "0x6ae73b50ffe305118883a1259134ebb574a2793958cb2c1bec33b7db03a5017e",
     },
     testnet: {
-        // LOL. Change SPAM_MODULE and SPAM_SYMBOL below for this to work.
-        packageId: "0x9d26aeba82a31aa1e322ce0d1c5f6cb79c7d6708a2ab2e7c3ad22350287cd093",
-        directorId: "0xda93b051883818115bfc8edd74814acc3dbd6c53073393e9db31fc4a7226d8be",
+        packageId: "0xee8c6eef6ccfec35841924540e84fa6fb066596862503dbeef190eef5eab198b",
+        directorId: "0x8fc17669386e9f033069d138e83c0d66b34f77624924538563edeb73dfedf948",
     },
     devnet: {
-        // LOL. Change SPAM_MODULE and SPAM_SYMBOL below for this to work.
-        packageId: "0x4e826283a47fcbc3264b93b27708ec9d3ec603b3bf4f0a7bf593e900b15d392b",
-        directorId: "0x10b48b8854980935bcc5d404497e7788d555c3f063c77b23e6a94edd0075c6cb",
+        packageId: "0x6643583a9b194516985434f5479f3c3d7435cb09ca0a7c26ee472134140cf313",
+        directorId: "0x549f43498c2337e8dec316b46748d089a37be11bf8e2ac63718e64069d41cea1",
     },
     localnet: {
-        packageId: "0x48815f75e8a5389f412dcb6b95eb49ac082f6043f4d4c2437355913830335ccf",
-        directorId: "0x00268a2806153cf83e50498b58bbf19d470b09a03a7801953a12a631acc35f3a",
+        packageId: "",
+        directorId: "",
     },
 };
 
-export const SPAM_MODULE = "lol";
-export const SPAM_SYMBOL = "LOL";
+export const SPAM_MODULE = "spam";
+export const SPAM_SYMBOL = "SPAM";
 export const SPAM_DECIMALS = 4;
 export const SUI_DECIMALS = 9;
