522243d70e58d9f89e701453808808ac61db66f2
juzybits <96890594+juzybits@users.noreply.github.com>
2024-05-02 08:37:20 +0000

    /home: link to README.md

--- src/web/src/PageHome.tsx
+++ src/web/src/PageHome.tsx
@@ -12,17 +12,23 @@ export const PageHome: React.FC = () =>
         </p>
 
         <br/>
+        <Link className="btn" to="/spam">
+            START
+        </Link>
+
+        <br/><br/>
         <h3>ELI5</h3>
         <div className="tight">
             <p>- One billion SPAM coins are minted every day.</p>
-            <p>- You can earn SPAM simply by sending transactions.</p>
+            <p>- You earn SPAM simply by sending Sui transactions.</p>
             <p>- The more txs you send, the more SPAM you receive.</p>
             <p>- There is no proof of work, only proof of spam.</p>
         </div>
 
         <br/>
-        <Link className="btn" to="/spam">
-            START
-        </Link>
+
+        <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/juzybits/polymedia-spam/blob/main/README.md">
+            Read the docs
+        </a></p>
     </>;
 };
