3016bb47126b270b847682d5613de4a3651d83ed
juzybits <96890594+juzybits@users.noreply.github.com>
2024-05-03 21:07:11 +0000

    web: .text-[red|orange|green]

--- src/web/src/components/StatusSpan.tsx
+++ src/web/src/components/StatusSpan.tsx
@@ -8,9 +8,9 @@ export const StatusSpan: React.FC<{
     if (!status) {
         return <span>loading</span>;
     }
-    let color: string;
-    if (status === "stopped") { color = "rgb(255 130 130)"; } // red
-    else if (status === "stopping") { color = "rgb(255 190 71)"; } // orange
-    else { color = "lightgreen"; }
-    return <span style={{color}} >{status}</span>;
+    let className: string;
+    if (status === "stopped") { className = "text-red"; }
+    else if (status === "stopping") { className = "text-orange"; }
+    else { className = "text-green"; }
+    return <span className={className} >{status}</span>;
 };
