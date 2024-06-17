fc3ae1da66dd2542ed630d92c049767f3a3c0871
juzybits <96890594+juzybits@users.noreply.github.com>
2024-06-17 09:07:15 +0000

    tweaks

--- src/sdk/src/package.ts
+++ src/sdk/src/package.ts
@@ -7,100 +7,100 @@ import {
 import { SPAM_MODULE } from "./config.js";
 
 export function new_user_counter(
-    txb: Transaction,
+    tx: Transaction,
     packageId: string,
     directorId: string,
 ): TransactionResult {
-    return txb.moveCall({
+    return tx.moveCall({
         target: `${packageId}::${SPAM_MODULE}::new_user_counter`,
         arguments: [
-            txb.object(directorId),
+            tx.object(directorId),
         ],
     });
 }
 
 export function increment_user_counter(
-    txb: Transaction,
+    tx: Transaction,
     packageId: string,
     userCounterRef: SuiObjectRef,
 ): TransactionResult {
-    return txb.moveCall({
+    return tx.moveCall({
         target: `${packageId}::${SPAM_MODULE}::increment_user_counter`,
         arguments: [
-            txb.objectRef(userCounterRef),
+            tx.objectRef(userCounterRef),
         ],
     });
 }
 
 export function destroy_user_counter(
-    txb: Transaction,
+    tx: Transaction,
     packageId: string,
     userCounterId: string,
 ): TransactionResult {
-    return txb.moveCall({
+    return tx.moveCall({
         target: `${packageId}::${SPAM_MODULE}::destroy_user_counter`,
         arguments: [
-            txb.object(userCounterId),
+            tx.object(userCounterId),
         ],
     });
 }
 
 export function register_user_counter(
-    txb: Transaction,
+    tx: Transaction,
     packageId: string,
     directorId: string,
     userCounterId: string,
 ): TransactionResult {
-    return txb.moveCall({
+    return tx.moveCall({
         target: `${packageId}::${SPAM_MODULE}::register_user_counter`,
         arguments: [
-            txb.object(directorId),
-            txb.object(userCounterId),
+            tx.object(directorId),
+            tx.object(userCounterId),
         ],
     });
 }
 
 export function claim_user_counter(
-    txb: Transaction,
+    tx: Transaction,
     packageId: string,
     directorId: string,
     userCounterId: string,
 ): TransactionResult {
-    return txb.moveCall({
+    return tx.moveCall({
         target: `${packageId}::${SPAM_MODULE}::claim_user_counter`,
         arguments: [
-            txb.object(directorId),
-            txb.object(userCounterId),
+            tx.object(directorId),
+            tx.object(userCounterId),
         ],
     });
 }
 
 export function stats_for_specific_epochs(
-    txb: Transaction,
+    tx: Transaction,
     packageId: string,
     directorId: string,
     epochNumbers: number[],
 ): TransactionResult {
-    return txb.moveCall({
+    return tx.moveCall({
         target: `${packageId}::${SPAM_MODULE}::stats_for_specific_epochs`,
         arguments: [
-            txb.object(directorId),
-            txb.pure(bcs.vector(bcs.U64).serialize(epochNumbers)),
+            tx.object(directorId),
+            tx.pure(bcs.vector(bcs.U64).serialize(epochNumbers)),
         ],
     });
 }
 
 export function stats_for_recent_epochs(
-    txb: Transaction,
+    tx: Transaction,
     packageId: string,
     directorId: string,
     epochCount: number,
 ): TransactionResult {
-    return txb.moveCall({
+    return tx.moveCall({
         target: `${packageId}::${SPAM_MODULE}::stats_for_recent_epochs`,
         arguments: [
-            txb.object(directorId),
-            txb.pure.u64(epochCount),
+            tx.object(directorId),
+            tx.pure.u64(epochCount),
         ],
     });
 }
