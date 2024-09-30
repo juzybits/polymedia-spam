9c4d7bc9925fa7f39af6651edd2b1352a687ead8
juzybits <96890594+juzybits@users.noreply.github.com>
2024-09-30 09:08:40 +0000

    pnpm add -D -w eslint-plugin-import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { fixupConfigRules } from "@eslint/compat";
 import eslint from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
+import eslintPluginImport from "eslint-plugin-import";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import tsEslint from "typescript-eslint";
 
@@ -22,6 +23,7 @@ export default [
         },
         plugins: {
             "@stylistic": stylistic,
+            import: eslintPluginImport,
         },
         rules: {
             "@stylistic/jsx-quotes": [ "error", "prefer-double" ],
@@ -37,6 +39,7 @@ export default [
             "@typescript-eslint/no-namespace": "off",
             "@typescript-eslint/no-non-null-assertion": "off",
             "@typescript-eslint/no-unnecessary-condition": "off",
+            "@typescript-eslint/no-unnecessary-type-parameters": "off",
             "@typescript-eslint/no-unused-expressions": [ "error", { allowShortCircuit: true, allowTernary: true } ],
             "@typescript-eslint/no-unused-vars": [ "error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" } ],
             "@typescript-eslint/prefer-nullish-coalescing": "off",
@@ -52,6 +55,18 @@ export default [
             react: {
                 version: "18"
             }
+        },
+    },
+    {
+        files: ["src/core/**/*.ts?(x)", "src/sdk/**/*.ts?(x)"],
+        rules: {
+            "import/extensions": ["error", "ignorePackages", { ts: "never", tsx: "never" }],
+        },
+    },
+    {
+        files: ["**/__tests__/**/*.ts?(x)"],
+        rules: {
+            "import/extensions": ["off", "ignorePackages"],
         },
     },
 ];
